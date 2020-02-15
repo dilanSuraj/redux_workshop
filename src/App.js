ecaab145aae6ea42b7cf6aa86ddda9b044f3645d
dilanSuraj <it17121002@my.sliit.lk>
2020-02-15 20:25:48 +0530

    redux added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { counter, logged } from './actions';
+
 
 function App() {
+  const counterVal = useSelector(state => state.counter)
+  const isLoggedVal = useSelector(state => state.logged)
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Counter {counterVal}
+      </h1>
+      <div>
+        <button onClick={ () => dispatch(counter.increment()) }>Increment</button>
+        <button onClick={ () => dispatch(counter.decrement()) }>Decrement</button>
+        <button onClick={ () => dispatch(counter.multiply()) }>Multply</button>
+      </div>
+
+      <div>
+        {
+          isLoggedVal ? 
+          <button onClick={ () => dispatch(logged.logout()) }>Log out</button> : 
+          <button onClick={ () => dispatch(logged.signIn()) }>Sign In</button>
+        }
+
+      </div>
+
     </div>
+
   );
 }
 
